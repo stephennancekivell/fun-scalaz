7895e6277a065761bd6b42c190594e48bd657548
Stephen <stephennancekivell@gmail.com>
2016-06-26 13:40:50 +1000

    init

---
+++ build.sbt
@@ -0,0 +1,10 @@
+scalaVersion := "2.11.8"
+
+name := "fun-scalaz"
+
+version := "1.0-SNAPSHOT"
+
+libraryDependencies ++= Seq(
+  "org.scalaz" %% "scalaz-core" % "7.2.4",
+  "org.scalatest" %% "scalatest" % "2.2.5" % "test"
+)
---
+++ src/test/scala/fun/EitherTSpec.scala
@@ -0,0 +1,32 @@
+package fun
+
+import org.scalatest.FreeSpec
+import org.scalatest.concurrent.ScalaFutures
+
+import scalaz._
+import Scalaz._
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits._
+
+class EitherTSpec extends FreeSpec with ScalaFutures {
+
+  case class ErrorMsg(msg: String)
+
+  type Result = Either[ErrorMsg,String]
+
+  def maybeFind(a:String): Future[\/[ErrorMsg,String]] = Future.successful(-\/(ErrorMsg("couldnt find "+a)))
+
+  val result = for {
+    a <- EitherT(maybeFind("a"))
+    b <- EitherT(maybeFind(a+"a"))
+  } yield b
+
+  val out: Future[\/[ErrorMsg, String]] = result.run
+
+  "EitherT" in {
+    whenReady(out) { found =>
+      assertResult(-\/(ErrorMsg("couldnt find a")))(found)
+    }
+  }
+}
